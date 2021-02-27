aa05846ca56bc4a13846c64cd4bec56500262548
Stéferson Augusto <steferson_a@hotmail.com>
2021-02-27 06:38:41 -0400

    feat: realiza conexão como bot do discord

--- discord-bot/src/index.ts
+++ discord-bot/src/index.ts
@@ -1,1 +1,10 @@
-console.log('teste')
+import Discord from 'discord.js'
+require('dotenv').config()
+
+const client = new Discord.Client()
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client?.user?.tag}!`)
+})
+
+client.login(process.env.TOKEN)
