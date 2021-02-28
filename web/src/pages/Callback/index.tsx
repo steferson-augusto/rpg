b42040a720268c3c28d5cb2756fd24dd5c2adf22
Stéferson Augusto <steferson_a@hotmail.com>
2021-02-28 19:29:22 -0400

    feat(web): adiciona animação ao falhar/efetuar login

--- web/src/pages/Callback/index.tsx
+++ web/src/pages/Callback/index.tsx
@@ -1,9 +1,12 @@
-import React, { useEffect } from 'react'
+/* eslint-disable multiline-ternary */
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import { useLocation } from 'react-router-dom'
+import Lottie from 'lottie-web'
 
-import Animation from '../../components/Animation'
 import { useAuth } from '../../contexts/auth'
-import { Container } from './styles'
+import Animation from '../../components/Animation'
+import { BackgroundAnimation, Absolute } from './styles'
+import Unauthorized from '../../components/Unauthorized'
 
 const useQuery = () => {
   return new URLSearchParams(useLocation().search)
@@ -12,22 +15,47 @@ const useQuery = () => {
 const Callback: React.FC = () => {
   const query = useQuery()
   const { signin } = useAuth()
+  const [loading, setLoading] = useState(true)
+  const [error, seterror] = useState(false)
+  const ref = useRef(null)
+
+  useEffect(() => {
+    if (!loading) {
+      Lottie.loadAnimation({
+        container: (ref.current as unknown) as Element,
+        loop: false,
+        renderer: 'svg',
+        autoplay: true,
+        animationData: require('../../assets/animations/login_background.json'),
+        rendererSettings: {
+          preserveAspectRatio: 'xMinYMin slice'
+        }
+      })
+    }
+  }, [loading])
+
+  const onError = useCallback(() => {
+    setLoading(false)
+    setTimeout(() => {
+      seterror(true)
+    }, 1000)
+  }, [])
 
   useEffect(() => {
     const code = query.get('code')
-    if (code) {
-      signin(code)
-        .then(response => console.log(response))
-        .catch(error => console.log(error))
-    } else {
-      console.log('código inválido')
-    }
+    console.log(code)
+    setTimeout(() => {
+      setLoading(false)
+    }, 750)
+    signin(`${code}`).catch(() => onError())
   }, [])
 
   return (
-    <Container>
-      <Animation width="50vh" height="50vh" />
-    </Container>
+    <BackgroundAnimation ref={ref}>
+      <Absolute>
+        {error ? <Unauthorized /> : <Animation width="50vh" height="50vh" />}
+      </Absolute>
+    </BackgroundAnimation>
   )
 }
 
