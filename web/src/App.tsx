01e4fb3a31227dcc99e1794740a63ddbac2c8de1
Stéferson Augusto <steferson_a@hotmail.com>
2021-02-27 22:36:05 -0400

    fix: corrige localização dos providers

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import { ThemeProvider } from 'styled-components'
+import {
+  StylesProvider,
+  ThemeProvider as MuiThemeProvider
+} from '@material-ui/core/styles'
 
+import GlobalStyles from './styles/global'
+import theme from './styles/theme'
 import Routes from './routes'
+import { AuthProvider } from './contexts/auth'
 
-const App: React.FC = () => <Routes />
+const App: React.FC = () => {
+  return (
+    <MuiThemeProvider theme={theme}>
+      <ThemeProvider theme={theme}>
+        <StylesProvider injectFirst>
+          <GlobalStyles />
+          <AuthProvider>
+            <Routes />
+          </AuthProvider>
+        </StylesProvider>
+      </ThemeProvider>
+    </MuiThemeProvider>
+  )
+}
 
 export default App
