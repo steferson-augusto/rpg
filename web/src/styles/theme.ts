eaeb8c14603e00e99f22ca2a0d2f3b81cc1f1271
Stéferson Augusto <steferson_a@hotmail.com>
2021-02-28 19:18:46 -0400

    fix(web): altera tema padrão para "dark"

--- web/src/styles/theme.ts
+++ web/src/styles/theme.ts
@@ -2,28 +2,23 @@ import { createMuiTheme } from '@material-ui/core/styles'
 
 const theme = createMuiTheme({
   palette: {
-    background: {
-      default: '#f5f5f5'
-    },
+    type: 'dark',
     primary: {
       main: '#5e35b1'
     },
     secondary: {
       main: '#018786'
-    },
-    text: {
-      primary: '#333'
     }
   },
   typography: {
     htmlFontSize: 10
   },
   overrides: {
-    MuiDrawer: {
-      paper: {
-        background: '#E5E5E5'
-      }
-    }
+    // MuiDrawer: {
+    //   paper: {
+    //     background: '#E5E5E5'
+    //   }
+    // }
   }
 })
 
