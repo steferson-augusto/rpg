335665866f0d6731452526d0db1e9cc6df4a5c98
Stéferson Augusto <steferson_a@hotmail.com>
2021-03-02 02:36:09 -0400

    feat(web): configura estrutura como global

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,14 +1,25 @@
+/* eslint-disable multiline-ternary */
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 
 import AppRoutes from './app.routes'
 import { useAuth } from '../contexts/auth'
 import AuthRoutes from './auth.routes'
+import Structure from '../components/Structure'
 
 const Routes: React.FC = () => {
   const { signed } = useAuth()
+
   return (
-    <BrowserRouter>{signed ? <AppRoutes /> : <AuthRoutes />}</BrowserRouter>
+    <BrowserRouter>
+      {signed ? (
+        <Structure>
+          <AppRoutes />
+        </Structure>
+      ) : (
+        <AuthRoutes />
+      )}
+    </BrowserRouter>
   )
 }
 
