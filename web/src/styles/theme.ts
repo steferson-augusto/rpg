c7da09c74ec327f0472bc4280fc64fc732439df1
Stéferson Augusto <steferson_a@hotmail.com>
2021-03-02 02:37:01 -0400

    feat(web): adiciona cores a tema padrão

--- web/src/styles/theme.ts
+++ web/src/styles/theme.ts
@@ -1,13 +1,31 @@
 import { createMuiTheme } from '@material-ui/core/styles'
 
+const primary = '#5e35b1'
+const secondary = '#018786'
+
 const theme = createMuiTheme({
   palette: {
     type: 'dark',
     primary: {
-      main: '#5e35b1'
+      main: primary
     },
     secondary: {
-      main: '#018786'
+      main: secondary
+    },
+    header: {
+      main: '#1F1F1F',
+      dark: '#1F1F1F',
+      light: primary
+    },
+    sidebar: {
+      main: '#333333',
+      dark: '#333333',
+      light: '#E5E5E5'
+    },
+    bg: {
+      main: '#121212',
+      dark: '#121212',
+      light: '#f5f5f5'
     }
   },
   typography: {
