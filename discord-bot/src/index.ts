5397b2d23a9a30ee2d4ecd970cabfe01fac1c2ce
Stéferson Augusto <steferson_a@hotmail.com>
2021-03-10 23:56:55 -0400

    feat(bot): adiciona roles na API

--- discord-bot/src/index.ts
+++ discord-bot/src/index.ts
@@ -1,10 +1,37 @@
-import Discord from 'discord.js'
+import { Client, Intents } from 'discord.js'
+import api from './services/api'
 require('dotenv').config()
 
-const client = new Discord.Client()
+const intents = new Intents(Intents.NON_PRIVILEGED)
+intents.add('GUILD_MEMBERS')
 
-client.on('ready', () => {
+const client = new Client({ ws: { intents } })
+
+client.on('ready', async () => {
+  const guild = client.guilds.cache.get(process.env.GUILD_ID as string)
+  const members = await guild?.members.fetch()
+  if (members) {
+    for (const member of members) {
+      const [id, user] = member
+      const isPlayer = user.roles.cache.has(
+        process.env.PLAYER_ROLE_ID as string
+      )
+      const isMaster = user.roles.cache.has(
+        process.env.MASTER_ROLE_ID as string
+      )
+      const data = {
+        discordId: id,
+        username: user.user.username,
+        avatar: user.user.avatar,
+        isPlayer,
+        isMaster
+      }
+
+      if (isPlayer || isMaster) await api.post('/users', data)
+    }
+  }
+
   console.log(`Logged in as ${client?.user?.tag}!`)
 })
 
-client.login(process.env.TOKEN)
+client.login(process.env.DISCORD_TOKEN)
