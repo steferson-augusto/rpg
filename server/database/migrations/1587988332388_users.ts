4cc3e0965ccbb398992d3cffc9ab91fd989319f6
Stéferson Augusto <steferson_a@hotmail.com>
2021-03-11 00:06:21 -0400

    fix(api): atualiza migration de usuários com roles

--- server/database/migrations/1587988332388_users.ts
+++ server/database/migrations/1587988332388_users.ts
@@ -6,8 +6,11 @@ export default class UsersSchema extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
-      table.string('discord_id', 255).notNullable()
+      table.string('discord_id', 255).unique().notNullable()
       table.string('username', 255).notNullable()
+      table.boolean('is_player').notNullable().defaultTo(false)
+      table.boolean('is_master').notNullable().defaultTo(false)
+      table.boolean('is_bot').notNullable().defaultTo(false)
       table.string('avatar', 255).nullable()
       table.string('email', 255).nullable()
       table.string('password', 180).nullable()
