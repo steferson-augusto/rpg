b1fe87a92ae219aaf0c1bf2bb3165ce49b51a221
Stéferson Augusto <steferson_a@hotmail.com>
2021-03-14 01:23:07 -0400

    feat(api): remove atributos de model User ao serializar

--- server/app/Models/User.ts
+++ server/app/Models/User.ts
@@ -30,13 +30,13 @@ export default class User extends BaseModel {
   @column({ serializeAs: null })
   public password: string
 
-  @column()
+  @column({ serializeAs: null })
   public rememberMeToken?: string
 
-  @column.dateTime({ autoCreate: true })
+  @column.dateTime({ autoCreate: true, serializeAs: null })
   public createdAt: DateTime
 
-  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  @column.dateTime({ autoCreate: true, autoUpdate: true, serializeAs: null })
   public updatedAt: DateTime
 
   @beforeSave()
