74c546b0bf3c92635110a6758eba3692c2767469
Stéferson Augusto <steferson_a@hotmail.com>
2021-04-22 01:55:28 -0400

    feat(web): cria página de macros

---
+++ web/src/pages/Macro/styles.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .search {
+    display: flex;
+    align-items: center;
+    padding: 4px 8px;
+  }
+
+  .content {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    padding-top: 12px;
+
+    .card {
+      margin: 4px 6px;
+      img {
+        max-width: 130px;
+        max-height: 130px;
+      }
+    }
+  }
+`
