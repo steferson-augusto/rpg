edfcf4b9c85fe2ef15eeb2b942bf2d81a07c1728
Stéferson Augusto <steferson_a@hotmail.com>
2021-04-25 17:58:50 -0400

    feat(web): adiciona autocomplete e inserção pelo mestre

--- web/src/pages/Skills/FormSkill/index.tsx
+++ web/src/pages/Skills/FormSkill/index.tsx
@@ -1,13 +1,17 @@
-import React, { useCallback, useRef, useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import { FormHandles } from '@unform/core'
 import { Button, CircularProgress } from '@material-ui/core'
 
 import { Container } from './styles'
-import Input from '../../../components/Input'
 import InputDices from '../../../components/InputDices'
+import Input from '../../../components/Input'
 import Checkbox from '../../../components/Checkbox'
 import api from '../../../services/api'
 import Skill from '../../../models/Skill'
+import Autocomplete from '../../../components/Autocomplete'
+import useSwr from '../../../hooks/useSWR'
+import { useAuth } from '../../../contexts/auth'
+import { usePlayer } from '../../../contexts/player'
 
 interface FormSkillProps {
   handleCancel: (() => void) | undefined
@@ -17,31 +21,50 @@ interface FormSkillProps {
 const FormSkill: React.FC<FormSkillProps> = ({ handleCancel, mutate }) => {
   const [loading, setLoading] = useState(false)
   const formRef = useRef<FormHandles>(null)
+  const { user } = useAuth()
+  const { selected } = usePlayer()
 
-  const handleSubmit = useCallback(async values => {
-    setLoading(true)
-    try {
-      const { data } = await api.post<Skill>('/skills', values)
-      mutate(data)
-      setLoading(false)
-      handleCancel?.()
-    } catch {
-      setLoading(false)
-      console.error('Não possível adicionar esta perícia')
-    }
+  const { data, loading: loadingSkills } = useSwr<Array<{ label: string }>>(
+    '/skills'
+  )
+
+  useEffect(() => {
+    const label = formRef?.current?.getFieldRef('label')
+    label?.focus?.()
   }, [])
+
+  const handleSubmit = useCallback(
+    async values => {
+      setLoading(true)
+      try {
+        if (user?.isMaster) {
+          values.userId = selected?.id
+        }
+        const { data } = await api.post<Skill>('/skills', values)
+        mutate(data)
+        setLoading(false)
+        handleCancel?.()
+      } catch {
+        setLoading(false)
+        console.error('Não possível adicionar esta perícia')
+      }
+    },
+    [user, selected]
+  )
 
   return (
     <Container ref={formRef} onSubmit={handleSubmit}>
       <div className="content">
         <h4>ADICIONAR PERÍCIA</h4>
-        <Input
+        <Autocomplete
           name="label"
           label="Nome da perícia"
-          variant="outlined"
-          required
+          loading={loadingSkills}
+          options={data?.map(({ label }) => ({ label, value: label })) || []}
           fullWidth
+          required
         />
+
         <InputDices
           name="dices"
           label="Dados"
@@ -50,14 +73,17 @@ const FormSkill: React.FC<FormSkillProps> = ({ handleCancel, mutate }) => {
           required
           fullWidth
         />
-        <Input
-          name="powerPoints"
-          label="Pontos de poder"
-          variant="outlined"
-          type="number"
-          min={0}
-          fullWidth
-        />
+        {user?.isMaster && (
+          <Input
+            name="powerPoints"
+            label="Pontos de poder"
+            type="number"
+            min={0}
+            defaultValue="0"
+            variant="outlined"
+            fullWidth
+          />
+        )}
         <Checkbox name="pinned" label="Favorito" fullWidth />
       </div>
 
@@ -73,7 +99,7 @@ const FormSkill: React.FC<FormSkillProps> = ({ handleCancel, mutate }) => {
           style={{ minWidth: 108 }}
         >
           {loading ? (
-            <CircularProgress size={24} color="primary" />
+            <CircularProgress size={24} color="secondary" />
           ) : (
             'Adicionar'
           )}
