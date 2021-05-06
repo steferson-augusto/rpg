118c25887d0f70a47561f5aad3fa05068e988d84
Stéferson Augusto <steferson_a@hotmail.com>
2021-05-05 22:59:17 -0400

    feat(web): inicia página de dashboard

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -1,10 +1,88 @@
-import React from 'react'
+import React, { useCallback, useRef } from 'react'
+import { Paper } from '@material-ui/core'
+
+import { Container } from './styles'
+import useSwr from '../../hooks/useSWR'
+import { usePlayer } from '../../contexts/player'
+import Animation from '../../components/Animation'
+import Pool from './Pool'
+import Stat from '../../models/Stat'
+import { AttributeData } from '../../models'
+import Dice from './Dice'
+import DialogRoll, {
+  DialogRollHandles,
+  DialogValues
+} from '../../components/DialogRoll'
 
 const Dashboard: React.FC = () => {
+  const dialogRef = useRef<DialogRollHandles>(null)
+  const { selected } = usePlayer()
+
+  const { data: character, loading: loadingCharacter } = useSwr(
+    `/character/user/${selected?.id}`
+  )
+
+  const { data: pools, loading: loadingPools } = useSwr<Stat[]>(
+    `/stats/user/${selected?.id}?energy=1`
+  )
+
+  const { data: attributes, loading: loadingAttributes } = useSwr<
+    AttributeData[]
+  >(`/attributes/${selected?.id}`)
+
+  const handleOpenDialog = useCallback(
+    (values: DialogValues) => {
+      dialogRef.current?.open(values)
+    },
+    [dialogRef]
+  )
+
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
+    <Container>
+      <div className="column">
+        <Paper className="character">
+          {loadingCharacter ? (
+            <Animation height="50px" />
+          ) : (
+            <>
+              <ul>
+                <li>{character?.name}</li>
+                <li>{character?.race}</li>
+              </ul>
+              <p>{character.xp} XP</p>
+            </>
+          )}
+        </Paper>
+        <Paper style={{ height: 200 }}>teste</Paper>
+      </div>
+      <div className="column">
+        <Paper className="pools">
+          {loadingPools ? (
+            <Animation height="100px" />
+          ) : (
+            pools?.map(pool => <Pool key={pool.id} data={pool} />)
+          )}
+        </Paper>
+      </div>
+      <div className="column">
+        <Paper>
+          {loadingAttributes ? (
+            <Animation height="100px" />
+          ) : (
+            attributes?.map(attribute => (
+              <Dice
+                key={attribute.id}
+                data={attribute}
+                handleOpenDialog={handleOpenDialog}
+              />
+            ))
+          )}
+        </Paper>
+        <Paper style={{ height: 200 }}>teste</Paper>
+        <div className="spacer" />
+      </div>
+      <DialogRoll ref={dialogRef} />
+    </Container>
   )
 }
 
---
+++ web/src/pages/Dashboard/styles.ts
@@ -0,0 +1,60 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  max-width: 800px;
+  padding: 0 16px;
+  margin: 16px auto;
+  display: flex;
+  flex-grow: 1;
+
+  .column {
+    flex: 1;
+    padding-bottom: 16px;
+
+    & + .column {
+      margin-left: 16px;
+    }
+
+    .MuiPaper-root {
+      margin-bottom: 16px;
+      padding: 8px;
+    }
+  }
+
+  .spacer {
+    width: 100%;
+    height: 1px;
+  }
+
+  .character {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
+
+    ul {
+      list-style-type: none;
+      color: var(--text-color);
+      padding-right: 8px;
+
+      & li:nth-child(1) {
+        font-size: 1.8rem;
+        font-weight: 600;
+      }
+    }
+
+    p {
+      font-size: 2rem;
+      font-weight: 800;
+      letter-spacing: 1px;
+      color: var(--primary);
+    }
+  }
+
+  .pools {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+`
