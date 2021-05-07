e41d0ccc131f68fc765cab44a3dce8d40f17978c
Stéferson Augusto <steferson_a@hotmail.com>
2021-05-06 22:16:11 -0400

    feat(api): cria hook para atualizar valor de stats
    adiciona hook para criar e atualizar valor de resistência e movimentação

--- server/app/Models/Attribute.ts
+++ server/app/Models/Attribute.ts
@@ -1,5 +1,8 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, afterSave, column } from '@ioc:Adonis/Lucid/Orm'
+
+import Stat from 'App/Models/Stat'
+import dicesToValue from 'App/Helpers/dicesToValue'
 
 export default class Attribute extends BaseModel {
   @column({ isPrimary: true })
@@ -16,6 +19,38 @@ export default class Attribute extends BaseModel {
     prepare: (value: string[]) => value.join(' ')
   })
   public dices: string[]
+
+  @afterSave()
+  public static async updateEndurance(attribute: Attribute) {
+    if (attribute.dices) {
+      if (attribute.label === 'Vigor') {
+        const current = Math.floor(dicesToValue(attribute.dices) / 2 + 2)
+        const endurance = await Stat.firstOrNew(
+          { userId: attribute.userId, label: 'resistência' },
+          { userId: attribute.userId, current, label: 'resistência' }
+        )
+
+        endurance.current = current
+        endurance.save()
+      }
+
+      const attributes = await Attribute.query().where({ userId: attribute.userId })
+      const sum = attributes.reduce<number>((sum, attr) => {
+        const value = dicesToValue(attr.dices)
+        return sum + value
+      }, 0)
+
+      const current = Math.floor(sum / 12)
+
+      const movement = await Stat.firstOrNew(
+        { userId: attribute.userId, label: 'movimentação' },
+        { userId: attribute.userId, current, label: 'movimentação' }
+      )
+
+      movement.current = current
+      movement.save()
+    }
+  }
 
   @column.dateTime({ autoCreate: true, serializeAs: null })
   public createdAt: DateTime
