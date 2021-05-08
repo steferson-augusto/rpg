3e09bd8370c118590ff15593444a0c6b805fc010
Stéferson Augusto <steferson_a@hotmail.com>
2021-05-07 23:11:23 -0400

    fix(api): corrige model e migration de vantagens
    cria tabela independente de usuário

--- server/app/Models/Advancement.ts
+++ server/app/Models/Advancement.ts
@@ -4,9 +4,6 @@ export default class Advancement extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
-  @column({ columnName: 'user_id', serializeAs: 'userId' })
-  public userId: number
-
   @column()
   public label: string
 
--- server/database/migrations/1620185590123_advancements.ts
+++ server/database/migrations/1620185590123_advancements.ts
@@ -6,7 +6,6 @@ export default class Advancements extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE')
       table.string('label', 24).notNullable()
       table.text('description').notNullable()
       table.boolean('hindrance').defaultTo(false)
