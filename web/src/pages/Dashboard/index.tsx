1b060e158ae291f5bcfb3b8f4ea2cd91e4e66a93
Stéferson Augusto <steferson_a@hotmail.com>
2021-05-08 22:34:40 -0400

    feat(web): utiliza cards e formulário de vantagens

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -1,5 +1,4 @@
 import React, { useCallback, useRef } from 'react'
-import { Paper } from '@material-ui/core'
 
 import { Container } from './styles'
 import Pool from './Pool'
@@ -7,10 +6,9 @@ import Stat from './Stat'
 import Dice from './Dice'
 import useSwr from '../../hooks/useSWR'
 import { usePlayer } from '../../contexts/player'
-import Animation from '../../components/Animation'
 import {
   AttributeData,
-  // AdvancementData,
+  AdvancementData,
   SkillData,
   StatData
 } from '../../models'
@@ -18,35 +16,47 @@ import DialogRoll, {
   DialogRollHandles,
   DialogValues
 } from '../../components/DialogRoll'
-// import Advancement from './Advancement'
+import Card from './Card'
+import Advancement from './Advancement'
+import Drawer, { DrawerHandles } from '../../components/Drawer'
+import FormAdvancement from './FormAdvancement'
+import api from '../../services/api'
 
 const Dashboard: React.FC = () => {
   const dialogRef = useRef<DialogRollHandles>(null)
+  const drawerRef = useRef<DrawerHandles>(null)
   const { selected } = usePlayer()
 
-  const { data: character, loading: loadingCharacter } = useSwr(
-    `/character/user/${selected?.id}`
-  )
+  const {
+    data: character,
+    loading: loadingCharacter,
+    error: errorCharacter
+  } = useSwr(`/character/user/${selected?.id}`)
 
-  const { data: pools, loading: loadingPools } = useSwr<StatData[]>(
-    `/stats/user/${selected?.id}?energy=1`
-  )
+  const { data: pools, loading: loadingPools, error: errorPools } = useSwr<
+    StatData[]
+  >(`/stats/user/${selected?.id}?energy=1`)
 
-  const { data: stats, loading: loadingStats } = useSwr<StatData[]>(
-    `/stats/user/${selected?.id}?energy=0`
-  )
+  const { data: stats, loading: loadingStats, error: errorStats } = useSwr<
+    StatData[]
+  >(`/stats/user/${selected?.id}?energy=0`)
 
-  // const { data: advancements, loading: loadingAdvancements } = useSwr<
-  //   AdvancementData[]
-  // >(`/advancements/user/${selected?.id}`)
+  const {
+    data: advancements,
+    loading: loadingAdvancements,
+    error: errorAdvancements,
+    mutate: mutateAdvancements
+  } = useSwr<AdvancementData[]>(`/advancements/user/${selected?.id}`)
 
-  const { data: attributes, loading: loadingAttributes } = useSwr<
-    AttributeData[]
-  >(`/attributes/${selected?.id}`)
+  const {
+    data: attributes,
+    loading: loadingAttributes,
+    error: errorAttributes
+  } = useSwr<AttributeData[]>(`/attributes/${selected?.id}`)
 
-  const { data: skills, loading: loadingskills } = useSwr<SkillData[]>(
-    `/skills/user/${selected?.id}?favorites=1`
-  )
+  const { data: skills, loading: loadingskills, error: errorSkills } = useSwr<
+    SkillData[]
+  >(`/skills/user/${selected?.id}?favorites=1`)
 
   const handleOpenDialog = useCallback(
     (values: DialogValues) => {
@@ -55,86 +65,130 @@ const Dashboard: React.FC = () => {
     [dialogRef]
   )
 
+  const handleOpenDrawer = useCallback(() => {
+    drawerRef.current?.open()
+  }, [drawerRef])
+
+  const mutateAdvancement = useCallback(() => {
+    mutateAdvancements(advancements, true)
+  }, [advancements])
+
+  const handleDeleteAdvancement = useCallback(
+    (id: number) => async () => {
+      try {
+        await api.delete(`/user/advancements/${id}`, {
+          data: { userId: selected?.id }
+        })
+        const newData = advancements?.filter(
+          advancement => advancement.id !== id
+        )
+        mutateAdvancements(newData, false)
+      } catch {
+        console.error('Falha ao remover vantagem deste usuário')
+      }
+    },
+    [advancements, selected]
+  )
+
   return (
     <Container>
       <div className="column">
-        <Paper className="character">
-          <h3 className="title">Personagem</h3>
-          {loadingCharacter ? (
-            <Animation height="50px" />
-          ) : (
-            <>
-              <ul>
-                <li>{character?.name}</li>
-                <li>{character?.race}</li>
-              </ul>
-              <p>{character?.xp} XP</p>
-            </>
-          )}
-        </Paper>
-        <Paper>
-          <h3 className="title">Status</h3>
-          {loadingStats ? (
-            <Animation height="100px" />
-          ) : (
-            stats?.map(stat => <Stat key={stat.id} data={stat} />)
-          )}
-        </Paper>
-        {/* <Paper className="advancements">
-          <h3 className="title">
-            <span>DES</span>Vantagens
-          </h3>
-          {loadingAdvancements ? (
-            <Animation height="100px" />
-          ) : (
-            advancements?.map(advancement => (
-              <Advancement key={advancement.id} data={advancement} />
-            ))
-          )}
-        </Paper> */}
+        <Card
+          className="character"
+          title="Personagem"
+          loading={loadingCharacter}
+          error={errorCharacter}
+          empty={false}
+          loadingHeight="50px"
+        >
+          <ul>
+            <li>{character?.name}</li>
+            <li>{character?.race}</li>
+          </ul>
+          <p>{character?.xp} XP</p>
+        </Card>
+        <Card
+          title="Status"
+          loading={loadingStats}
+          error={errorStats}
+          empty={stats?.length === 0}
+        >
+          {stats?.map(stat => (
+            <Stat key={stat.id} data={stat} />
+          ))}
+        </Card>
+        <Card
+          title="Vantagens"
+          preTitle="des"
+          loading={loadingAdvancements}
+          error={errorAdvancements}
+          empty={advancements?.length === 0}
+          action={{
+            icon: 'add',
+            onClick: handleOpenDrawer
+          }}
+        >
+          {advancements?.map(advancement => (
+            <Advancement
+              key={advancement.id}
+              data={advancement}
+              handleDelete={handleDeleteAdvancement}
+            />
+          ))}
+        </Card>
       </div>
       <div className="column">
-        <Paper className="pools">
-          <h3 className="title">Energias</h3>
-          {loadingPools ? (
-            <Animation height="100px" />
-          ) : (
-            pools?.map(pool => <Pool key={pool.id} data={pool} />)
-          )}
-        </Paper>
+        <Card
+          className="pools"
+          title="Energias"
+          loading={loadingPools}
+          error={errorPools}
+          empty={pools?.length === 0}
+        >
+          {pools?.map(pool => (
+            <Pool key={pool.id} data={pool} />
+          ))}
+        </Card>
       </div>
       <div className="column">
-        <Paper>
-          <h3 className="title">Atributos</h3>
-          {loadingAttributes ? (
-            <Animation height="100px" />
-          ) : (
-            attributes?.map(attribute => (
-              <Dice
-                key={attribute.id}
-                data={attribute}
-                handleOpenDialog={handleOpenDialog}
-              />
-            ))
-          )}
-        </Paper>
-        <Paper>
-          <h3 className="title">Perícias</h3>
-          {loadingskills ? (
-            <Animation height="100px" />
-          ) : (
-            skills?.map(skill => (
-              <Dice
-                key={skill.id}
-                data={skill}
-                handleOpenDialog={handleOpenDialog}
-              />
-            ))
-          )}
-        </Paper>
+        <Card
+          title="Atributos"
+          loading={loadingAttributes}
+          error={errorAttributes}
+          empty={attributes?.length === 0}
+        >
+          {attributes?.map(attribute => (
+            <Dice
+              key={attribute.id}
+              data={attribute}
+              handleOpenDialog={handleOpenDialog}
+            />
+          ))}
+        </Card>
+        <Card
+          title="Perícias"
+          loading={loadingskills}
+          error={errorSkills}
+          empty={skills?.length === 0}
+        >
+          {skills?.map(skill => (
+            <Dice
+              key={skill.id}
+              data={skill}
+              handleOpenDialog={handleOpenDialog}
+            />
+          ))}
+        </Card>
         <div className="spacer" />
       </div>
       <DialogRoll ref={dialogRef} />
+
+      <Drawer ref={drawerRef}>
+        <FormAdvancement
+          handleCancel={drawerRef.current?.close}
+          mutate={mutateAdvancement}
+        />
+      </Drawer>
     </Container>
   )
 }
