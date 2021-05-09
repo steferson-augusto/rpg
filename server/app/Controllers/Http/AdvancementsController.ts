5140da04f6c00bf9fb4c62224aef57ae71687ea0
Stéferson Augusto <steferson_a@hotmail.com>
2021-05-08 22:23:21 -0400

    fix(api): corrige busca em tabela many to many

--- server/app/Controllers/Http/AdvancementsController.ts
+++ server/app/Controllers/Http/AdvancementsController.ts
@@ -1,25 +1,29 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { schema as Schema, rules } from '@ioc:Adonis/Core/Validator'
+import Database from '@ioc:Adonis/Lucid/Database'
 import Advancement from 'App/Models/Advancement'
 
 const schema = Schema.create({
-  label: Schema.string({}, [rules.minLength(3), rules.maxLength(24)]),
-  description: Schema.string({}, [rules.minLength(4), rules.maxLength(264)]),
+  label: Schema.string({}, [rules.minLength(3), rules.maxLength(36)]),
+  description: Schema.string({}, [rules.minLength(4), rules.maxLength(300)]),
   hindrance: Schema.boolean()
 })
 
 const messages = {
   required: 'Campo obrigatório',
   minLength: 'Deve haver pelo menos {{ options.minLength }} caracteres',
-  maxLength: 'Deve haver pelo menos {{ options.maxLength }} caracteres'
+  maxLength: 'Deve {{ options.maxLength }} caracteres no máximo'
 }
 
 export default class AdvancementsController {
   public async getByUser({ auth, params }: HttpContextContract) {
     const userId = auth.user?.isMaster ? params.id : auth.user?.id
-    const advancements = await Advancement.query().where({ userId }).orderBy('label', 'asc')
+    const advancements = await Database.rawQuery(
+      'select * from advancements a where id in (select advancement_id from user_advancements ua where ua.user_id = ?) order by a.label ASC',
+      [userId]
+    )
 
-    return advancements
+    return advancements?.rows || []
   }
 
   public async index() {
