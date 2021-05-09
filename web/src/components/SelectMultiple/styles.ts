5130bc98a74152bf1cccd0262b6942e0ed0efd52
Stéferson Augusto <steferson_a@hotmail.com>
2021-05-08 22:26:31 -0400

    feat(web): cria componente de select múltiplo

---
+++ web/src/components/SelectMultiple/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Chips = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+
+  .chip {
+    background-color: var(--surface-2);
+    margin: 2px;
+  }
+`
