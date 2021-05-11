34d96fc444bf10971612c4714d99e91ca488d8e8
Stéferson Augusto <steferson_a@hotmail.com>
2021-05-10 23:17:24 -0400

    refactor(api): cria funções para atualizar mods

--- server/app/Models/UserAdvancement.ts
+++ server/app/Models/UserAdvancement.ts
@@ -1,8 +1,6 @@
 import { DateTime } from 'luxon'
 import { BaseModel, column, afterCreate, beforeDelete } from '@ioc:Adonis/Lucid/Orm'
-import Advancement from 'App/Models/Advancement'
-import Modifier from 'App/Models/Modifier'
-import Stat from 'App/Models/Stat'
+import { addModifier, removeModifier } from 'App/Helpers/updateModifier'
 
 export default class UserAdvancement extends BaseModel {
   @column({ isPrimary: true })
@@ -21,65 +19,8 @@ export default class UserAdvancement extends BaseModel {
   public updatedAt: DateTime
 
   @afterCreate()
-  public static async addModifier(ua: UserAdvancement) {
-    const advancement = await Advancement.find(ua.advancementId)
-    const label = advancement?.label?.toLowerCase()
-    const userId = ua.userId
-    if (label === 'bloquear') {
-      const stat = await Stat.firstOrCreate(
-        { userId, label: 'aparar' },
-        {
-          userId,
-          label: 'aparar',
-          current: 0
-        }
-      )
-      Modifier.create({ statId: stat.id, label: 'Vantagem (bloquear)', value: 1 })
-      return
-    }
-    if (label === 'bloquear aprimorado') {
-      const stat = await Stat.firstOrCreate(
-        { userId, label: 'aparar' },
-        {
-          userId,
-          label: 'aparar',
-          current: 0
-        }
-      )
-      Modifier.create({ statId: stat.id, label: 'Vantagem (bloquear aprimorado)', value: 2 })
-      return
-    }
-    if (label === 'feio') {
-      const stat = await Stat.firstOrCreate(
-        { userId, label: 'carisma' },
-        {
-          userId,
-          label: 'carisma',
-          current: 0
-        }
-      )
-      Modifier.create({ statId: stat.id, label: 'Complicação (feio)', value: -2 })
-    }
-  }
+  public static addModifier = addModifier
 
   @beforeDelete()
-  public static async removeModifier(ua: UserAdvancement) {
-    const advancement = await Advancement.find(ua.advancementId)
-    const label = advancement?.label?.toLowerCase()
-    const userId = ua.userId
-    if (label === 'bloquear') {
-      const stat = await Stat.query().where({ userId, label: 'aparar' }).firstOrFail()
-      Modifier.query().where({ statId: stat?.id, label: 'Vantagem (bloquear)' }).delete()
-      return
-    }
-    if (label === 'bloquear aprimorado') {
-      const stat = await Stat.query().where({ userId, label: 'aparar' }).firstOrFail()
-      Modifier.query().where({ statId: stat?.id, label: 'Vantagem (bloquear aprimorado)' }).delete()
-      return
-    }
-    if (label === 'feio') {
-      const stat = await Stat.query().where({ userId, label: 'carisma' }).firstOrFail()
-      Modifier.query().where({ statId: stat?.id, label: 'Complicação (feio)' }).delete()
-    }
-  }
+  public static removeModifier = removeModifier
 }
