c1d79c2bb3f31ba257130daa5ee59a7dd4f1d0a4
Stéferson Augusto <steferson_a@hotmail.com>
2021-05-10 23:20:38 -0400

    feat(api): cria controller para gerenciar status

--- server/app/Controllers/Http/StatsController.ts
+++ server/app/Controllers/Http/StatsController.ts
@@ -1,6 +1,25 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Stat from 'App/Models/Stat'
+import { schema as Schema, rules } from '@ioc:Adonis/Core/Validator'
 
+const schema = {
+  create: Schema.create({
+    label: Schema.string({}, [rules.minLength(3), rules.maxLength(24)]),
+    current: Schema.number(),
+    userId: Schema.number([rules.exists({ table: 'users', column: 'id' })])
+  }),
+  update: Schema.create({
+    label: Schema.string({}, [rules.minLength(3), rules.maxLength(24)]),
+    current: Schema.number()
+  })
+}
+
+const messages = {
+  required: 'Campo obrigatório',
+  existis: 'Este usuário não existe',
+  minLength: 'Deve haver pelo menos {{ options.minLength }} caracteres',
+  maxLength: 'Támanho máximo é {{ options.minLength }} caracteres'
+}
 export default class StatsController {
   public async getByUser({ auth, params, request }: HttpContextContract) {
     const userId = auth.user?.isMaster ? params.id : auth.user?.id
@@ -12,5 +31,33 @@ export default class StatsController {
       .orderBy(energy === undefined ? 'label' : 'id', 'asc')
 
     return stats
+  }
+
+  public async store({ request }: HttpContextContract) {
+    const data = await request.validate({ schema: schema.create, messages })
+
+    const stat = await Stat.create(data)
+    return stat
+  }
+
+  public async update({ request, params, bouncer }: HttpContextContract) {
+    const data = await request.validate({ schema: schema.update, messages })
+
+    const stat = await Stat.find(params.id)
+    await bouncer.authorize('founded', stat)
+
+    stat?.merge(data)
+    await stat?.save()
+
+    return stat
+  }
+
+  public async destroy({ params, response, bouncer }: HttpContextContract) {
+    const stat = await Stat.find(params.id)
+
+    await bouncer.authorize('founded', stat)
+
+    await stat?.delete()
+    return response.status(204)
   }
 }
