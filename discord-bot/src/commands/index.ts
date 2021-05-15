22d7a6617d61b9349850327d152047499caa3fff
Stéferson Augusto <steferson_a@hotmail.com>
2021-05-14 23:07:37 -0400

    feat(bot): agrupa funções de comando

---
+++ discord-bot/src/commands/index.ts
@@ -0,0 +1,13 @@
+import roll from './roll'
+import mroll from './mroll'
+import ring from './ring'
+import weaponSmithy from './weaponSmithy'
+
+const commands = {
+  roll,
+  mroll,
+  anel: ring,
+  'forja-arma': weaponSmithy
+}
+
+export default commands
