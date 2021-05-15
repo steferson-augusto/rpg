50aef6dea0a20fc0c864c9c28bb89bb14a65f95c
Stéferson Augusto <steferson_a@hotmail.com>
2021-05-15 19:56:13 -0400

    feat(bot): cria comando para rolar atributo

--- discord-bot/src/commands/index.ts
+++ discord-bot/src/commands/index.ts
@@ -1,12 +1,14 @@
 import roll from './roll'
 import mroll from './mroll'
 import ring from './ring'
+import attribute from './attribute'
 import weaponSmithy from './weaponSmithy'
 
 const commands = {
   roll,
   mroll,
   anel: ring,
+  atributo: attribute,
   'forja-arma': weaponSmithy
 }
 
