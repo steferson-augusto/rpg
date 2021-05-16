7b04e0873bc051b850582db39f0b10776d3114ce
Stéferson Augusto <steferson_a@hotmail.com>
2021-05-15 20:33:49 -0400

    feat(api): dispara hook ao adicionar usuário

--- server/app/Models/User.ts
+++ server/app/Models/User.ts
@@ -1,6 +1,10 @@
 import { DateTime } from 'luxon'
 import Hash from '@ioc:Adonis/Core/Hash'
-import { column, beforeSave, BaseModel } from '@ioc:Adonis/Lucid/Orm'
+import { column, beforeSave, afterCreate, BaseModel } from '@ioc:Adonis/Lucid/Orm'
+import Attribute from 'App/Models/Attribute'
+import Skill from 'App/Models/Skill'
+import Stat from './Stat'
+import Character from './Character'
 
 export default class User extends BaseModel {
   @column({ isPrimary: true })
@@ -44,5 +48,35 @@ export default class User extends BaseModel {
     if (user.$dirty.password) {
       user.password = await Hash.make(user.password)
     }
+  }
+
+  @afterCreate()
+  public static async createAttributes(user: User) {
+    const userId = user.id
+    Attribute.createMany([
+      { userId, label: 'Agilidade', dices: ['d4'] },
+      { userId, label: 'Astúcia', dices: ['d4'] },
+      { userId, label: 'Espírito', dices: ['d4'] },
+      { userId, label: 'Força', dices: ['d4'] },
+      { userId, label: 'Vigor', dices: ['d4'] }
+    ])
+
+    Skill.create({
+      label: 'lutar',
+      userId,
+      powerPoints: 0,
+      pinned: false,
+      dices: ['d4']
+    })
+
+    Stat.createMany([
+      { userId, label: 'hp', max: 4, current: 4, energy: true },
+      { userId, label: 'fadiga', max: 4, current: 4, energy: true },
+      { userId, label: 'mana', max: 4, current: 4, energy: true },
+      { userId, label: 'ki', max: 10, current: 10, energy: true },
+      { userId, label: 'movimentação', current: 0 }
+    ])
+
+    Character.create({ userId, name: 'Sem nome', race: 'Anão', xp: 0 })
   }
 }
